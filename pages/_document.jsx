821d4fb7257dff774c79a003f3fbdf7592bd29d1
Tauseef Shah <tauseefsshah@gmail.com>
2024-07-22 18:48:01 +0530

    WIP - Home

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -5,7 +5,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="body-container bg-[#161616] text-white">
+      <body className="body-container bg-primary-background text-white">
         <header className="content-box">
           <div className="flex justify-between items-center">
             <div
@@ -16,19 +16,19 @@ export default function Document() {
             </div>
             <nav id="logo">
               <ul className="flex gap-4">
-                <li className="p-2 rounded-full hover:bg-[#363636]">
+                <li className="p-2 rounded-full hover:bg-primary">
                   <Home className="h-5 w-5" />
                 </li>
-                <li className="p-2 rounded-full hover:bg-[#363636]">
+                <li className="p-2 rounded-full hover:bg-primary">
                   <UserCircle className="h-5 w-5" />
                 </li>
-                <li className="p-2 rounded-full hover:bg-[#363636]">
+                <li className="p-2 rounded-full hover:bg-primary">
                   <AppWindowMac className="h-5 w-5" />
                 </li>
               </ul>
             </nav>
             <div id="cta">
-              <div className="p-2 rounded-full hover:bg-[#363636]">
+              <div className="p-2 rounded-full hover:bg-primary">
                 <Inbox className="h-5 w-5" />
               </div>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,13 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: {
+          DEFAULT: "#363636",
+          "muted": "#212121",
+          "background": "#161616",
+        }
+      }
     },
   },
   plugins: [],
