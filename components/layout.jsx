7c1b2aad482eb94fc6bce939187f9007e4eeb0f1
Tauseef Shah <tauseefsshah@gmail.com>
2024-07-23 00:58:41 +0530

    REFACTOR - To layout

---
+++ components/layout.jsx
@@ -0,0 +1,120 @@
+import Link from "next/link";
+import { AppWindowMac, Home, Inbox, UserCircle } from "lucide-react";
+import {
+  SiDiscord,
+  SiGithub,
+  SiLinkedin,
+  SiSignal,
+  SiTelegram,
+  SiX,
+} from "@icons-pack/react-simple-icons";
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <header className="content-box">
+        <div className="flex justify-between items-center">
+          <div
+            id="logo"
+            className="bg-white text-black rounded-full p-2 font-bold"
+          >
+            TS
+          </div>
+          <nav id="logo">
+            <ul className="flex gap-4">
+              {[
+                {
+                  title: "Home",
+                  icon: <Home className="h-5 w-5" />,
+                  link: "/",
+                  active: false,
+                },
+                {
+                  title: "About",
+                  icon: <UserCircle className="h-5 w-5" />,
+                  link: "/about",
+                  active: false,
+                },
+                {
+                  title: "Projects",
+                  icon: <AppWindowMac className="h-5 w-5" />,
+                  link: "/projects",
+                  active: false,
+                },
+              ].map((navigationItem) => (
+                <Link key={navigationItem.title} href={navigationItem.link}>
+                  <li
+                    title={navigationItem.title}
+                    className="p-2 rounded-full hover:bg-primary"
+                  >
+                    {navigationItem.icon}
+                  </li>
+                </Link>
+              ))}
+            </ul>
+          </nav>
+          <div id="cta">
+            <div className="p-2 rounded-full hover:bg-primary">
+              <Link href="/contact">
+                <Inbox className="h-5 w-5" />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </header>
+
+      <main>{children}</main>
+
+      <footer className="content-box">
+        <div className="flex flex-col gap-4 items-center">
+          <div className="flex gap-4">
+            {[
+              {
+                platform: "x",
+                link: "https://x.com/tauseefsshah",
+                icon: <SiX className="h-5 w-5" />,
+              },
+              {
+                platform: "linkedin",
+                link: "https://linkedin.com/in/tauseefsshah",
+                icon: <SiLinkedin className="h-5 w-5" />,
+              },
+              {
+                platform: "github",
+                link: "https://github.com/tauseefsshah",
+                icon: <SiGithub className="h-5 w-5" />,
+              },
+              {
+                platform: "discord",
+                link: "https://discordapp.com/users/742727242823499847",
+                icon: <SiDiscord className="h-5 w-5" />,
+              },
+              {
+                platform: "signal",
+                link: "https://signal.me/#u/tauseefsshah.01",
+                icon: <SiSignal className="h-5 w-5" />,
+              },
+              {
+                platform: "telegram",
+                link: "https://t.me/tauseefsshah",
+                icon: <SiTelegram className="h-5 w-5" />,
+              },
+            ].map((socialMedia) => (
+              <a
+                key={socialMedia.platform}
+                href={socialMedia.link}
+                target="_blank"
+                title={socialMedia.platform}
+              >
+                {socialMedia.icon}
+              </a>
+            ))}
+          </div>
+          <div className="text-sm uppercase">
+            &copy; {new Date().getFullYear()}. Tauseef Shah
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+}
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,3 +1,4 @@
+import Layout from "@/components/layout";
 import "@/styles/globals.css";
 import { Rubik } from "next/font/google";
 
@@ -8,13 +9,13 @@ const rubik = Rubik({
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <Layout>
       <style jsx global>{`
         html {
           font-family: ${rubik.style.fontFamily};
         }
       `}</style>
       <Component {...pageProps} />
-    </>
+    </Layout>
   );
 }
