7a5327c5da55171e66392a3fd109e77e02cffb48
Tauseef Shah <tauseefsshah@gmail.com>
2024-07-22 14:22:21 +0530

    CREATE - Initial UI

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
 import "@/styles/globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  weight: ["400", "700"],
+  subsets: ['latin']
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>{`
+      html {
+        font-family: ${inter.style.fontFamily};
+      }
+    `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,41 @@
+import { AppWindowMac, Home, Inbox, UserCircle } from "lucide-react";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className="body-container bg-[#161616] text-white">
+        <header className="content-box">
+          <div className="flex justify-between items-center">
+            <div id="logo" className="bg-white text-black rounded-full p-2">
+              TS
+            </div>
+            <nav id="logo">
+              <ul className="flex gap-4">
+                <li className="p-2 rounded-full hover:bg-[#363636]">
+                  <Home className="h-5 w-5" />
+                </li>
+                <li className="p-2 rounded-full hover:bg-[#363636]">
+                  <UserCircle className="h-5 w-5" />
+                </li>
+                <li className="p-2 rounded-full hover:bg-[#363636]">
+                  <AppWindowMac className="h-5 w-5" />
+                </li>
+              </ul>
+            </nav>
+            <div id="cta">
+              <div className="p-2 rounded-full hover:bg-[#363636]">
+                <Inbox className="h-5 w-5" /></div>
+            </div>
+          </div>
+        </header>
         <Main />
+        <footer className="content-box">
+          <div className="text-center text-sm uppercase">
+            &copy; {new Date().getFullYear()}. Tauseef Shah
+          </div>
+        </footer>
         <NextScript />
       </body>
     </Html>
