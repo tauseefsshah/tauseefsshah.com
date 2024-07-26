9caeb9451ad15ac67fa5d1c5828d2d22a5d00995
Tauseef Shah <tauseefsshah@gmail.com>
2024-07-26 20:06:56 +0530

    ADD - Google analytics

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    GOOGLE_ANALYTICS_ID: process.env.GOOGLE_ANALYTICS_ID,
+  }
 };
 
 export default nextConfig;
