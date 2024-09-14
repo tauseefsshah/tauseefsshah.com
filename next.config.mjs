da0f70a5b3862c4148420f5754b6f825a02307ef
Tauseef Shah <tauseefsshah@gmail.com>
2024-09-14 11:25:58 +0530

    WIP

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    GOOGLE_ANALYTICS_ID: process.env.GOOGLE_ANALYTICS_ID,
-  }
-};
-
-export default nextConfig;
