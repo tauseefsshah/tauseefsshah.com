62e5527e02e1e0ad2ffc78672c63458d4ed44ec0
Tauseef Shah <tauseefsshah@gmail.com>
2025-01-26 14:09:32 +0530

    chore: create blog component

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,40 +1,10 @@
-import Link from "next/link";
+import Blog from "@/components/blog";
 
-import fs from "fs";
-import path from "path";
-import matter from "gray-matter";
-
-export default function Blog() {
-  const files = fs.readdirSync(path.join("data", "blogs"));
-
-  const blogs = files.map((file) => {
-    const slug = file.replace(".md", "");
-
-    const meta = matter(fs.readFileSync(path.join("data", "blogs", file))).data;
-
-    return {
-      slug,
-      meta,
-    };
-  });
-
-  blogs.sort((a, b) => new Date(b.meta.date) - new Date(a.meta.date));
+export default function Page() {
 
   return (
-    <main className="space-y-20 py-20 max-w-screen-sm mx-auto">
-      <div className="space-y-8">
-        {blogs.map((blog) => (
-          <div key={blog.slug} className="space-y-4">
-            <div className="space-y-2">
-              <Link href={`/blog/${blog.slug}`}>
-                <h2 className="text-2xl font-semibold">{blog.meta.title}</h2>
-              </Link>
-              <p className="font-semiold text-slate-600">{blog.meta.date}</p>
-            </div>
-            <p className="text-slate-600 line-clamp-2">{blog.meta.excerpt}</p>
-          </div>
-        ))}
-      </div>
+    <main className="py-20 max-w-screen-sm mx-auto">
+      <Blog />
     </main>
   );
 }
--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ import Image from "next/image";
 
 import { ArrowUpRight } from "lucide-react";
 import Project from "@/components/project";
+import Blog from "@/components/blog";
 
 export default function Home() {
   const projects = allProjects.slice(0, 4);
@@ -51,65 +52,6 @@ export default function Home() {
         </div>
       </section>
 
-      {/* <section id="blog-posts">
-        <h1 className="mb-8 text-4xl font-bold">Blog Posts</h1>
-        <div className="space-y-8">
-          {[
-            {
-              title:
-                "B2B Flight Inventory Management And Distribution Platform",
-              stack: [
-                "Laravel",
-                "MySQL",
-                "React",
-                "TailwindCSS",
-                "InertiaJS",
-                "AlpineJS",
-                "DigitalOcean",
-              ],
-              description:
-                "Web based flight inventory management application called Flynterest Manager which is used by travel agents to manage their pre purchased flight inventory. The same inventory is aggreagated and distributed through Flynterest Marketplace.",
-            },
-            {
-              title: "Superset Complaints Dashboard For Ahmedabad Smart City",
-              stack: ["Python", "Postgres", "Apache Superset", "Pandas", "AWS"],
-              description:
-                "Apache superset based dashboard to show complaints data for grievances registered in the Ahmedabad Smart City. The dashboard is used by the Ahmedabad Municipal Corportation to generate actionable insights.",
-            },
-            {
-              title: "Wedding Cards Portfolio",
-              stack: ["React", "NextJS", "TailwindCSS", "Vercel"],
-              description:
-                "Wedding potfolio website for a digital printing and wedding studio. The website uses google sheets as a db to retrive the porfolio wedding cards.",
-            },
-          ].map((project, projectIndex) => (
-            <div key={projectIndex} className="content-box">
-              <div className="flex flex-col gap-4">
-                <h3 className="text-md font-semibold uppercase">
-                  {project.title}
-                </h3>
-                <ul className="flex flex-wrap gap-2 text-xs uppercase">
-                  {project.stack.map((stack, stackIndex) => (
-                    <li
-                      key={stackIndex}
-                      className="rounded-xl bg-slate-100 px-2 py-1"
-                    >
-                      {stack}
-                    </li>
-                  ))}
-                </ul>
-                <p className="text-sm">{project.description}</p>
-              </div>
-            </div>
-          ))}
-          <div>
-            <Link className="flex gap-1 items-center" href="/about">
-              Read More <ArrowUpRight className="size-4" />
-            </Link>
-          </div>
-        </div>
-      </section> */}
-
       <section id="projects">
         <h1 className="mb-8 text-4xl font-bold">Projects</h1>
         <div className="grid grid-cols-1 gap-8 md:grid-cols-2">
@@ -119,6 +61,19 @@ export default function Home() {
           <div>
             <Link className="flex gap-1 items-center" href="/projects">
               Know More <ArrowUpRight className="size-4" />
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      <section id="blog">
+        <h1 className="mb-8 text-4xl font-bold">Blog</h1>
+        <div className="space-y-8">
+          <Blog limit={3} />
+
+          <div>
+            <Link className="flex gap-1 items-center" href="/about">
+              Read More <ArrowUpRight className="size-4" />
             </Link>
           </div>
         </div>
