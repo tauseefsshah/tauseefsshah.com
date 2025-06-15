51aa4df0ef3cc2d5542711fb4fc3d6a293c85539
Tauseef Shah <tauseefsshah@gmail.com>
2025-06-15 17:55:49 +0530

    wip

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -9,7 +9,6 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { ChevronDownIcon } from "lucide-react";
 
 interface INavigationSubItem {
   link: string;
