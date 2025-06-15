a1460326d603a0003f182673939b48db9f64449c
Tauseef Shah <tauseefsshah@gmail.com>
2025-06-15 16:42:23 +0530

    refactor: code for blog post page

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,14 +1,18 @@
 import fs from "fs";
 import path from "path";
-import { marked } from "marked";
-import matter from "gray-matter";
 
-interface BlogMeta {
+import matter from "gray-matter";
+import { marked } from "marked";
+
+interface IPageParams {
+  params: Promise<{ slug: string }>;
+}
+interface IBlogMeta {
   title: string;
   date: string;
 }
 
-export async function generateStaticParams() {
+export function generateStaticParams() {
   const files = fs.readdirSync(path.join("src", "data", "blogs"));
 
   const slugs = files.map((file) => {
@@ -20,17 +24,22 @@ export async function generateStaticParams() {
   }));
 }
 
-export default async function Page({ params }: any) {
-  const { slug } = params;
+export default async function Page({ params }: IPageParams) {
+  const { slug } = await params;
 
-  const fileContent = fs.readFileSync(path.join("src", "data", "blogs", slug + ".md"));
+  const fileContent = fs.readFileSync(
+    path.join("src", "data", "blogs", slug + ".md")
+  );
   const { data, content } = matter(fileContent);
-  const meta = data as BlogMeta;
+  const meta = data as IBlogMeta;
 
   const _content = marked(content);
 
   return (
-    <main id="blog-content" className="space-y-8 py-20 max-w-[--breakpoint-sm] mx-auto">
+    <main
+      id="blog-content"
+      className="space-y-8 py-20 max-w-[--breakpoint-sm] mx-auto"
+    >
       <div>
         <h1 className="text-7xl font-black">{meta.title}</h1>
         <p className="text-xl font-semibold">{meta.date}</p>
